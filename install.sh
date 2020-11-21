9ded30154e70b7a762bf67ecc9a73186caca44d7
Alexander <info@webmanufaktur.net>
2020-11-21 19:15:59 +0100

    Added installer script

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Starting STOW..."
+echo "################"
+echo " "
+echo "-> Restoring alacritty..."
+stow alacritty
+echo "-> Restoring bash..."
+stow bash
+echo "-> Restoring compton..."
+stow compton
+echo "-> Restoring i3..."
+stow i3
+echo "-> Restoring imwheel..."
+stow imwheel
+echo "-> Restoring mpd..."
+stow mpd
+echo "-> Restoring ncmpcpp..."
+stow ncmpcpp
+echo "-> Restoring neofetch..."
+stow neofetch
+echo "-> Restoring newsboat..."
+stow newsboat
+echo "-> Restoring nvim..."
+stow nvim
+echo "-> Restoring picom..."
+stow picom
+echo "-> Restoring profile..."
+stow profile
+echo "-> Restoring qutebrowser..."
+stow qutebrowser
+echo "-> Restoring sxhkd..."
+stow sxhkd
+echo "-> Restoring termite..."
+stow termite
+echo "-> Restoring vim..."
+stow vim
+echo "-> Restoring xbindkeysrc..."
+stow xbindkeysrc
+echo " "
+echo "################"
+echo "STOW finished..."
